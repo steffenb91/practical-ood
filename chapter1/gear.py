5308b65dc918313a552c2ff2a0e5af560c843b98
Steffen Börner <steffenb1991.8@gmail.com>
2022-02-13 21:11:04 +0100

    what vs how

--- chapter1/gear.py
+++ chapter1/gear.py
@@ -2,7 +2,7 @@ from wheel import Wheel
 
 class Gear:
 
-    def __init__(self, chainring, cog, wheel) -> None:
+    def __init__(self, chainring = 40, cog = 18, wheel = None) -> None:
         self.chainring = chainring
         self.cog = cog
         self.wheel = wheel
--- chapter1/test_gear.py
+++ chapter1/test_gear.py
@@ -3,6 +3,5 @@ from gear import Gear
 from wheel import Wheel
 
 def test_gear():
-    wheel = Wheel(26, 1.5)
-    gear = Gear(52, 11, wheel)
+    gear = Gear(chainring = 52, cog = 11, wheel = Wheel(26, 1.5))
     assert gear.gear_inches() == 137.0909090909091
